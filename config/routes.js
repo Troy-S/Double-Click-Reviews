50c37a1d8c1303de9671c34889ba0be298802c7d
Troy-S <tsheed@hotmail.co.uk>
2015-08-25 19:21:11 +0100

    added secret to routes

--- config/routes.js
+++ config/routes.js
@@ -3,3 +3,14 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var passport = require('passport');
+var UsersController = require('../controllers/users');
+var GamesController = require('../controllers/games');
+
+function authenticatedUser(req, res, next) {
+  if (req.isAuthenticated()) return next();
+
+  res.redirect('/');
+}
+
+router.route('/secret')
+  .get(authenticatedUser, usersController.secret)
